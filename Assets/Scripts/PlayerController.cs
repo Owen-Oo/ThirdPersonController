589c63a08d16ba73cc42412e107258cacfb5dd72
Owen-Oo <o.oorebeek@gmail.com>
2025-02-27 09:00:29 -0800

    Implemented basic jumping mechanics
    No check for if player is touching the ground yet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,26 @@
+using System;
 using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
-    [SerializeField] private float speed;
     [SerializeField] private CinemachineCamera freeLookCamera;
 
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
+
     private Rigidbody playerRB;
+    private Boolean canJump;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        canJump = true;
 
         inputManager.OnMove.AddListener(MovePlayer);
         inputManager.OnSpacePressed.AddListener(Jump);
+        
     }
 
     void Update()
@@ -26,12 +32,17 @@ public class PlayerController : MonoBehaviour
 
     private void MovePlayer(Vector2 direction)
     {
-        Vector3 moveDirection = new(direction.x, 0f, direction.y);
+        Vector3 moveDirection = new(direction.x, playerRB.linearVelocity.y/5, direction.y);
         playerRB.linearVelocity = transform.rotation * moveDirection * speed;
     }
 
     private void Jump()
     {
-
+        if (canJump)
+        {
+            Vector3 jumpDirection = new(0f, 1f, 0f);
+            playerRB.AddForce(jumpDirection * jumpHeight);
+            //canJump = false;
+        }
     }
 }
